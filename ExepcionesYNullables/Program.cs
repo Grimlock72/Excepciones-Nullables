e9ac37ce937ac00eb96741b0f2ee470a160b892c
Juan Carlos Cuellar Ruz <juan-carlos.cuellar@atos.net>
2022-03-28 13:46:01 -0600

    Heredando de NullReferenceException.

---
+++ ExepcionesYNullables/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExepcionesYNullables
+{
+    class Program
+    {
+        public static List<string> miLista;
+        static Exception miExcepcion = new Exception("Revisa tu código!");
+
+        static void Main(string[] args)
+        {
+            int? number = 45;
+            number = null;
+            //miLista = new List<string>();
+            try
+            {
+                MiMetodo();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Algo salió mal: {0}", ex.Message);
+            }
+            Console.ReadLine();
+        }
+
+        private static void MiMetodo()
+        {
+            throw miExcepcion;
+        }
+    }
+}
